ba5d713f06b02fe9d1ed7ec817b049b23c463869
unknown <adiaz.mis2016@student.ie.edu>
2019-04-11 13:17:28 +0200

    User Class

--- src/User.java
+++ src/User.java
@@ -1,21 +1,60 @@
 public class User {
 
-    enum Gender {Male, Female};
+
     String name;
     String emailAddress;
     int age;
     String address;
     String city;
     String zipCode;
-    Gender gender;
 
-    public User(String name, String emailAddress, int age, String address, String city, String zipCode, Gender gender) {
+    public User setName (String name){
         this.name = name;
-        this.emailAddress = emailAddress;
-        this.age = age;
-        this.address = address;
-        this.city = city;
-        this.zipCode = zipCode;
-        this.gender = gender;
+        return this;
     }
+
+    public User setEmailAddress(String emailAddress){
+        this.emailAddress = emailAddress;
+        return this;
+    }
+
+    public User setAge(int age){
+        this.age = age;
+        return this;
+    }
+
+    public User setAddress(String address){
+        this.address = address;
+        return this;
+    }
+
+    public User setCity(String city){
+        this.city = city;
+        return this;
+    }
+
+    public User setZipCode(String zipCode){
+        this.zipCode = zipCode;
+        return this;
+    }
+
+
+
+class UserDemo {
+    public void main(String args[])
+    {
+        User user1 = new User();
+        User user2 = new User();
+
+        user1.setName("Jose").setEmailAddress("jose123@gmail.com").setAge(30).setAddress("Maria de Molina 20").setCity("Madrid").setZipCode("3342");
+        user1.setName("Maria").setEmailAddress("maria123@gmail.com").setAge(22).setAddress("Velazquez 14").setCity("Madrid").setZipCode("3342");
+
+        System.out.println(user1);
+        System.out.println(user2);
+    }
+}
+
+
+
+
 }
